4a6d7e1f697f0127a0e7819f9a883375f169af5c
chuy-zip <andreschuy03@gmail.com>
2024-04-30 18:56:38 -0600

    updated a message

--- games-blog/src/components/Auth.jsx
+++ games-blog/src/components/Auth.jsx
@@ -6,7 +6,8 @@ function Auth() {
             <p>In order to see what I and others think are the best games to play with friends or
                 by yourself, please register on the sign In section. That way you will be up to date
                 with the best game recommendations and will be able to create, update and delete 
-                them. Thanks again for your interest! Help us create a friendly community.
+                them. Thanks again for your interest! Help us create a friendly community. If you
+                do not see the login button, then press logout
             </p>
 
         </div>
