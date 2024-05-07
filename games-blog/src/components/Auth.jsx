3b903dc73bad4705800ab2326cf2e69f4d75d4c1
chuy-zip <andreschuy03@gmail.com>
2024-05-06 21:08:54 -0600

    new release

--- games-blog/src/components/Auth.jsx
+++ games-blog/src/components/Auth.jsx
@@ -4,9 +4,8 @@ function Auth() {
             <h1>🌀 Welcome! Thanks for your interest in our recommendations</h1>
             <br/>
             <p style={{margin: 0}}>In order to see what I and others think are the best games to play with friends or
-                by yourself, please register on the sign In section. That way you will be up to date
-                with the best game recommendations and will be able to create, update and delete 
-                them. Thanks again for your interest!
+                by yourself, please register on the sign In section. If your session expired and you are unable to login, press 
+                "logout". After register you will be able to create, update and delete games. Thanks!
             </p>
 
         </div>
